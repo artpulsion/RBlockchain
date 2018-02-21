8a514edb0d1a0f7a10c096b5538730c1c14dc5f4
artpulsion <mbarki_sofiane@yahoo.fr>
2018-02-21 18:08:06 +0000

    add plot

---
+++ README.md
@@ -0,0 +1,1 @@
+The main goal was to introduce how a blockchain looks like and introduce some of the core concepts behind it. 
--- block-chain.R
+++ block-chain.R
@@ -61,7 +61,7 @@ blockchain <- list(block_genesis)
 previous_block <- blockchain[[1]]
 
 # How many blocks should we add to the chain after the genesis block
-num_of_blocks_to_add <- 30
+num_of_blocks_to_add <- 19
 
 # Add blocks to the chain
 for (i in 1: num_of_blocks_to_add){
